420a3b7205fd43b23656c1ec5bdc282b36d8eab8
Tim Wee <tim.shawn@gmail.com>
2024-10-05 23:58:59 -0400

    add byte pair merge, test more encode

--- src/bpe_naive.rs
+++ src/bpe_naive.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 use crate::registry::{Rank, CL100K_BASE_SPECIAL_TOKENS, CL100K_BASE_PAT};
 use phf::phf_map;
-use regex::Regex;
+// use regex::Regex;
 use crate::io::read_bpe_openai;
-use fancy_regex;
+use fancy_regex::Regex;
 
 struct BPENaive<'a, 'b> {
     vocab: HashMap<Vec<u8>, Rank>,
@@ -24,24 +24,98 @@ impl<'a, 'b> BPENaive<'a, 'b> {
         }
     }
 
+    // Note that we hash bytes when indexing into `vocab`, not token pairs. As long as we train BPE
+    // the way we currently do, this is equivalent. An easy way to break this would be to decouple
+    // merge priority from token index or to prevent specific token merges.
+    // Given a piece, returns the (start, Rank) of each BPE token in the piece.
+    fn _byte_pair_merge(&self, piece: &[u8]) -> Vec<(usize, Rank)> {
+
+      // find the rank of the byte pair starting at each byte, RANK_MAX if not in vocab.
+      let mut parts: Vec<(usize, Rank)> = Vec::with_capacity(piece.len() + 1);
+      let mut min_rank: (Rank, usize) = (Rank::MAX, usize::MAX);
+      for i in 0..piece.len() - 1 {
+          let rank = *self.vocab.get(&piece[i..i + 2]).unwrap_or(&Rank::MAX);
+          if rank < min_rank.0 {
+              min_rank = (rank, i);
+          }
+          parts.push((i, rank));
+      }
+      parts.push((piece.len() - 1, Rank::MAX));
+      parts.push((piece.len(), Rank::MAX));
+
+      let get_rank = {
+        #[inline(always)]
+        |parts: &Vec<(usize, Rank)>, i: usize| {
+            if (i + 3) < parts.len() {
+                // Similar to `piece[i..i + 2]` above. The +3 is because we haven't yet deleted
+                // parts[i + 1], see comment in the main loop.
+                // We use the .0 (usize) field because there may be merged elements already
+                //    so we may need the whole subpiece.
+                *self.vocab
+                    .get(&piece[parts[i].0..parts[i + 3].0])
+                    .unwrap_or(&Rank::MAX)
+            } else {
+                Rank::MAX
+            }
+        }
+      };
+
+      // Run the merge loop until we can't merge any more.
+
+      // If you have n parts and m merges, this does O(mn) work.
+      // We could do something with a heap and do O(m log n) work.
+      // n is often very small so considerations like cache-locality outweigh the algorithmic
+      // complexity downsides of the `parts` vector.
+      while min_rank.0 != Rank::MAX {
+        // Do the merge for the current min ranking pair by removing [i + 1]th element
+        //   and updating the i-th and (i-1)th elements.
+        // The pair at these 2 positions are now:
+        // - (i-1)th: The i-1th element + the merged i-th and (i+1)th elements
+        // - ith: The merged i-th and (i+1)th elements, and the (i+2)th element
+        let i = min_rank.1;
+        // Update parts[i] and parts[i - 1] before removing parts[i + 1], since
+        // `parts.remove(i + 1)` will thrash the cache.
+        if i > 0 {
+          parts[i - 1].1 = get_rank(&parts, i - 1);
+        }
+        parts[i].1 = get_rank(&parts, i);
+        parts.remove(i + 1);
+
+        // find the next candidate to merge
+        min_rank = (Rank::MAX, usize::MAX);
+        for (i, &(_, rank)) in parts[..parts.len() - 1].iter().enumerate() {
+          if rank < min_rank.0 {
+            min_rank = (rank, i);
+          }
+        }
+      }
+      parts
+    }
+
+    // If the piece does not exist in the vocab, we need to find a tokenization
+    //  of the piece, accounting for byte pairs, and return the ranks of the tokens.
     fn _byte_pair_encode(&self, piece: &[u8]) -> Vec<Rank> {
-      assert!(piece.len() > 0);
-      todo!()
+      assert!(piece.len() > 1);
+      self._byte_pair_merge(piece)
+        .windows(2)
+        .map(|parts| self.vocab[&piece[parts[0].0..parts[1].0]])
+        .collect()
     }
 
     fn encode(&self, text: &str) -> Vec<Rank> {
-        let mut values = Vec::new();
-        self.split_pat.find_iter(text).for_each(|m| {
-            let k = m.as_str().as_bytes();
+      let mut values = Vec::new();
+      self.split_pat.find_iter(text).for_each(|m| {
+        eprintln!("match: {:?}", m);
+        let k = m.expect("Failed to get match").as_str().as_bytes();
 
-            match self.vocab.get(k) {
-                Some(rank) => values.push(*rank),
-                None => {
-                    values.extend(&self._byte_pair_encode(k));
-                }
-            }
-        });
-        values
+        match self.vocab.get(k) {
+          Some(rank) => values.push(*rank),
+          None => {
+            values.extend(&self._byte_pair_encode(k));
+          }
+        }
+      });
+      values
     }
 
     fn decode(&self, ranks: &[Rank]) -> String {
@@ -59,5 +133,11 @@ mod tests {
       let bpe = BPENaive::new(vocab, &CL100K_BASE_SPECIAL_TOKENS, &CL100K_BASE_PAT);
       assert_eq!(bpe.encode("0"), vec![15]);
       assert_eq!(bpe.encode("rer"), vec![38149]);
+      assert_eq!(bpe.encode("'rer"), vec![2351, 81]);
+      assert_eq!(bpe.encode("today\n "), vec![31213, 198, 220]);
+      assert_eq!(bpe.encode("today\n \n"), vec![31213, 27907]);
+      assert_eq!(bpe.encode("today\n  \n"), vec![31213, 14211]);
+      assert_eq!(bpe.encode("hello world"), vec![15339, 1917]);
+      assert_eq!(bpe.encode("👍"), [9468, 239, 235]);
     }
 }
--- src/registry.rs
+++ src/registry.rs
@@ -1,5 +1,5 @@
 use phf::phf_map;
-use regex::Regex;
+use fancy_regex::Regex;
 pub type Rank = u32;
 use lazy_static::lazy_static;
 
@@ -16,5 +16,5 @@ pub static CL100K_BASE_SPECIAL_TOKENS: phf::Map<&'static str, Rank> = phf_map! {
 // in tiktoken
 // (?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+
 lazy_static! {
-  pub static ref CL100K_BASE_PAT: Regex = Regex::new(r"(?i:'(?:s|d|m|t|ll|ve|re))|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]|\s+$|\s+").unwrap();
+  pub static ref CL100K_BASE_PAT: Regex = Regex::new(r"'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+").unwrap();
 }
