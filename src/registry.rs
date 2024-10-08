7ef58d22abb04921594e1a585ab53f509b9bbf80
Tim Wee <tim.shawn@gmail.com>
2024-10-07 21:11:17 -0400

    ad comments to explain regex for c100k_base

--- src/registry.rs
+++ src/registry.rs
@@ -1,5 +1,5 @@
-use phf::phf_map;
 use fancy_regex::Regex;
+use phf::phf_map;
 pub type Rank = u32;
 use lazy_static::lazy_static;
 
@@ -11,8 +11,34 @@ pub static CL100K_BASE_SPECIAL_TOKENS: phf::Map<&'static str, Rank> = phf_map! {
     "<|endofprompt|>" => 100276,
 };
 
-// TODO: not sure if the regex is correct, the original is
-// r"(?i:'(?:s|d|m|t|ll|ve|re))|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]|\s+(!?\S)|\s+"
+// Here's a breakdown of the pattern:
+// 1. '(?i:[sdmt]|ll|ve|re):
+// Matches contractions starting with an apostrophe.
+// (?i:...) makes the match case-insensitive.
+// Matches 's, 'd, 'm, 't, 'll, 've, or 're.
+
+// 2. |[^\r\n\p{L}\p{N}]?+\p{L}+:
+// Matches any letter sequence, optionally preceded by a single character that is not a letter, number, or line break.
+// \p{L} represents any Unicode letter.
+// \p{N} represents any Unicode number.
+
+// 3. |\p{N}{1,3}:
+// Matches 1 to 3 consecutive numbers.
+
+// 4. | ?[^\s\p{L}\p{N}]++[\r\n]*:
+// Matches any sequence of characters that are not whitespace, letters, or numbers, optionally preceded by a space and followed by any number of line breaks.
+
+// 5. |\s*[\r\n]:
+// Matches any line break, optionally preceded by whitespace.
+
+// 6. |\s+(?!\S):
+// Matches one or more whitespace characters at the end of a string.
+
+// 7. |\s+:
+// Matches one or more whitespace characters.
+
+// This regex seems designed to tokenize text, breaking it into words, numbers, punctuation, and whitespace. It can handle contractions, numbers up to 3 digits, various punctuation, and different types of whitespace including line breaks.
+// The pattern uses alternation (|) to match any of these sub-patterns, effectively creating a tokenizer that can split text into various components.
 // in tiktoken
 // (?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+
 lazy_static! {
